d6771ff60e4afb4a2da305ff190db3aacb68df30
zyb <745433716@qq.com>
2017-10-21 17:36:14 +0800

    状态栏调色

--- src/UIStatus.cpp
+++ src/UIStatus.cpp
@@ -52,71 +52,119 @@ void UIStatus::draw()
 
     auto font = Font::getInstance();
     BP_Color color = { 255, 255, 255, 255 };
+	BP_Color color_name = { 249, 249, 199, 255 };
+	BP_Color color_ability1 = { 249, 146, 16, 255 };
+	BP_Color color_ability2 = { 236, 200, 40, 255 };
+	BP_Color color_red = { 216, 20, 24, 255 };
+	BP_Color color_magic = { 236, 200, 40, 255 };
+	BP_Color color_magic_level1 = { 253, 101, 101, 255 };
+	BP_Color color_magic_level2 = { 208, 152, 208, 255 };
+	BP_Color color_magic_empty = { 236, 200, 40, 76 };
+	BP_Color color_equip = { 165, 28, 218, 255 };
+	
     int font_size = 22;
 
     int x, y;
 
     x = x_ + 200;
     y = y_ + 50;
-    font->draw(convert::formatString("%s", role_->Name), 30, x - 10, y, color);
-    font->draw(convert::formatString("等級%7d", role_->Level), font_size, x, y + 50, color);
-    font->draw(convert::formatString("經驗%7d", role_->Exp), font_size, x, y + 75, color);
+    font->draw(convert::formatString("%s", role_->Name), 30, x - 10, y, color_name);
+	font->draw("等級", font_size, x, y + 50, color_ability1);
+    font->draw(convert::formatString("%5d", role_->Level), font_size, x + 66, y + 50, color_ability2);
+	font->draw("經驗", font_size, x, y + 75, color_ability1);
+	font->draw(convert::formatString("%5d", role_->Exp), font_size, x + 66, y + 75, color_ability2);
 
-    std::string str = "升級 ------";
+    std::string str = "";
+	font->draw("升級", font_size, x, y + 100, color_ability1);
+
     int exp_up = GameUtil::getLevelUpExp(role_->Level);
-    if (exp_up != INT_MAX)
-    {
-        str = convert::formatString("升級%7d", exp_up);
-    }
-    font->draw(str, font_size, x, y + 100, color);
-    font->draw(convert::formatString("生命%5d/%5d", role_->HP, role_->MaxHP), font_size, x + 175, y + 50, color);
-    font->draw(convert::formatString("內力%5d/%5d", role_->MP, role_->MaxMP), font_size, x + 175, y + 75, color);
-    font->draw(convert::formatString("體力%5d/%5d", role_->PhysicalPower, 100), font_size, x + 175, y + 100, color);
+	if (exp_up != INT_MAX)
+	{
+		str = convert::formatString("%7d", exp_up);
+	}
+	else
+	{
+		str = "------";
+	}
+    font->draw(str, font_size, x+55, y + 100, color_ability2);
+	font->draw("生命", font_size, x + 175, y + 50, color_ability1);
+	font->draw(convert::formatString("%5d/", role_->HP), font_size, x + 219, y + 50, color_ability2);
+	font->draw(convert::formatString("%5d", role_->MaxHP), font_size, x + 285, y + 50, color_ability2);
+	font->draw("內力", font_size, x + 175, y + 75, color_ability1);
+	font->draw(convert::formatString("%5d/", role_->MP), font_size, x + 219, y + 75, color_ability2);
+	font->draw(convert::formatString("%5d", role_->MaxMP), font_size, x + 285, y + 75, color_ability2);
+	font->draw("體力", font_size, x + 175, y + 100, color_ability1);
+	font->draw(convert::formatString("%5d/", role_->PhysicalPower), font_size, x + 219, y + 100, color_ability2);
+	font->draw(convert::formatString("%5d", 100), font_size, x + 285, y + 100, color_ability2);
+
 
     x = x_ + 20;
     y = y_ + 200;
 
-    font->draw(convert::formatString("攻擊%5d", role_->Attack), font_size, x, y, color);
-    font->draw(convert::formatString("防禦%5d", role_->Defence), font_size, x + 200, y, color);
-    font->draw(convert::formatString("輕功%5d", role_->Speed), font_size, x + 400, y, color);
+	font->draw("攻擊", font_size, x, y, color);
+    font->draw(convert::formatString("%5d", role_->Attack), font_size, x + 44, y, color_ability2);
+	font->draw("防禦", font_size, x + 200, y, color);
+    font->draw(convert::formatString("%5d", role_->Defence), font_size, x + 244, y, color_ability2);
+	font->draw("輕功", font_size, x + 400, y, color);
+    font->draw(convert::formatString("%5d", role_->Speed), font_size, x + 444, y, color_ability2);
 
-    font->draw(convert::formatString("醫療%5d", role_->Medcine), font_size, x, y + 25, color);
-    font->draw(convert::formatString("解毒%5d", role_->Detoxification), font_size, x + 200, y + 25, color);
-    font->draw(convert::formatString("用毒%5d", role_->UsePoison), font_size, x + 400, y + 25, color);
+	font->draw("醫療", font_size, x, y + 25, color);
+	font->draw(convert::formatString("%5d", role_->Medcine), font_size, x + 44, y + 25, color_ability2);
+	font->draw("解毒", font_size, x + 200, y + 25, color);
+	font->draw(convert::formatString("%5d", role_->Detoxification), font_size, x + 244, y + 25, color_ability2);
+	font->draw("用毒", font_size, x + 400, y + 25, color);
+	font->draw(convert::formatString("%5d", role_->UsePoison), font_size, x + 444, y + 25, color_ability2);
+
 
     x = x_ + 20;
     y = y_ + 270;
-    font->draw("技能", 25, x - 10, y, color);
-    font->draw(convert::formatString("拳掌%5d", role_->Fist), font_size, x, y + 30, color);
-    font->draw(convert::formatString("御劍%5d", role_->Sword), font_size, x, y + 55, color);
-    font->draw(convert::formatString("耍刀%5d", role_->Knife), font_size, x, y + 80, color);
-    font->draw(convert::formatString("特殊%5d", role_->Unusual), font_size, x, y + 105, color);
-    font->draw(convert::formatString("暗器%5d", role_->HiddenWeapon), font_size, x, y + 130, color);
+    font->draw("技能", 25, x - 10, y, color_red);
+
+	font->draw("拳掌", font_size, x, y + 30, color);
+	font->draw(convert::formatString("%5d", role_->Fist), font_size, x + 44, y + 30, color_ability2);
+	font->draw("御劍", font_size, x, y + 55, color);
+	font->draw(convert::formatString("%5d", role_->Sword), font_size, x + 44, y + 55, color_ability2);
+	font->draw("耍刀", font_size, x, y + 80, color);
+	font->draw(convert::formatString("%5d", role_->Knife), font_size, x + 44, y + 80, color_ability2);
+	font->draw("特殊", font_size, x, y + 105, color);
+	font->draw(convert::formatString("%5d", role_->Unusual), font_size, x + 44, y + 105, color_ability2);
+	font->draw("暗器", font_size, x, y + 130, color);
+	font->draw(convert::formatString("%5d", role_->HiddenWeapon), font_size, x + 44, y + 130, color_ability2);
 
     x = x_ + 220;
     y = y_ + 270;
-    font->draw("武學", 25, x - 10, y, color);
+    font->draw("武學", 25, x - 10, y, color_red);
     for (int i = 0; i < 10; i++)
     {
         auto magic = Save::getInstance()->getRoleLearnedMagic(role_, i);
         std::string str = "__________";
         if (magic)
         {
-            str = convert::formatString("%-12s%3d", magic->Name, role_->getRoleShowLearnedMagicLevel(i));
+			int x1 = x + i % 2 * 200;
+			int y1 = y + 30 + i / 2 * 25;
+			
+			str = convert::formatString("%s", magic->Name);
+			font->draw(str, font_size, x1, y1, color_magic);
+            str = convert::formatString("%3d", role_->getRoleShowLearnedMagicLevel(i));
+			font->draw(str, font_size, x1+100, y1, role_->getRoleShowLearnedMagicLevel(i)>9 ? color_magic_level1 : color_magic_level2);
         }
-        int x1 = x + i % 2 * 200;
-        int y1 = y + 30 + i / 2 * 25;
-        font->draw(str, font_size, x1, y1, color);
+		else
+		{
+			int x1 = x + i % 2 * 200;
+			int y1 = y + 30 + i / 2 * 25;
+			font->draw("__________", font_size, x1, y1, color_magic_empty);
+		}
+
     }
 
     x = x_ + 420;
     y = y_ + 445;
-    font->draw("修煉", 25, x - 10, y, color);
+    font->draw("修煉", 25, x - 10, y, color_red);
     auto book = Save::getInstance()->getItem(role_->PracticeItem);
     if (book)
     {
         TextureManager::getInstance()->renderTexture("item", book->ID, x, y + 30);
-        font->draw(convert::formatString("%s", book->Name), font_size, x + 90, y + 30, color);
+        font->draw(convert::formatString("%s", book->Name), font_size, x + 90, y + 30, color_magic);
         font->draw(convert::formatString("經驗%5d", role_->ExpForItem), 18, x + 90, y + 55, color);
         std::string str = "升級 ----";
         int exp_up = GameUtil::getFinishedExpForItem(role_, book);
@@ -129,28 +177,35 @@ void UIStatus::draw()
 
     x = x_ + 20;
     y = y_ + 445;
-    font->draw("武器", 25, x - 10, y, color);
+    font->draw("武器", 25, x - 10, y, color_red);
     auto equip0 = Save::getInstance()->getItem(role_->Equip0);
     if (equip0)
     {
         TextureManager::getInstance()->renderTexture("item", equip0->ID, x, y + 30);
-        font->draw(convert::formatString("%s", equip0->Name), font_size, x + 90, y + 30, color);
-        font->draw(convert::formatString("攻擊%+d", equip0->AddAttack), 18, x + 90, y + 55, color);
-        font->draw(convert::formatString("防禦%+d", equip0->AddDefence), 18, x + 90, y + 75, color);
-        font->draw(convert::formatString("輕功%+d", equip0->AddSpeed), 18, x + 90, y + 95, color);
+        font->draw(convert::formatString("%s", equip0->Name), font_size, x + 90, y + 30, color_magic);
+		font->draw("攻擊", 18, x + 90, y + 55, color);
+		font->draw(convert::formatString("%+d", equip0->AddAttack), 18, x + 126, y + 75, color_magic_level2);
+		font->draw("防禦", 18, x + 90, y + 75, color);
+		font->draw(convert::formatString("%+d", equip0->AddDefence), 18, x + 126, y + 95, color_magic_level2);
+		font->draw("輕功", 18, x + 90, y + 95, color);
+		font->draw(convert::formatString("%+d", equip0->AddSpeed), 18, x + 126, y + 55, color_magic_level2);
+
     }
 
     x = x_ + 220;
     y = y_ + 445;
-    font->draw("防具", 25, x - 10, y, color);
+    font->draw("防具", 25, x - 10, y, color_red);
     auto equip1 = Save::getInstance()->getItem(role_->Equip1);
     if (equip1)
     {
         TextureManager::getInstance()->renderTexture("item", equip1->ID, x, y + 30);
-        font->draw(convert::formatString("%s", equip1->Name), font_size, x + 90, y + 30, color);
-        font->draw(convert::formatString("攻擊%+d", equip1->AddAttack), 18, x + 90, y + 55, color);
-        font->draw(convert::formatString("防禦%+d", equip1->AddDefence), 18, x + 90, y + 75, color);
-        font->draw(convert::formatString("輕功%+d", equip1->AddSpeed), 18, x + 90, y + 95, color);
+        font->draw(convert::formatString("%s", equip1->Name), font_size, x + 90, y + 30, color_magic);
+		font->draw("攻擊", 18, x + 90, y + 55, color);
+		font->draw(convert::formatString("%+d", equip1->AddAttack), 18, x + 126, y + 75, color_magic_level2);
+		font->draw("防禦", 18, x + 90, y + 75, color);
+		font->draw(convert::formatString("%+d", equip1->AddDefence), 18, x + 126, y + 95, color_magic_level2);
+		font->draw("輕功", 18, x + 90, y + 95, color);
+		font->draw(convert::formatString("%+d", equip1->AddSpeed), 18, x + 126, y + 55, color_magic_level2);
     }
 }
 
