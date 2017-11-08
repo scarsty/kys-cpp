84db09ab55e163574b315685c386b09816e6cb6d
sty <scarsty@gmail.com>
2017-11-08 14:25:50 +0800

    日暮乡关何处是，烟波江上使人愁
    统一option设置

--- src/Option.cpp
+++ src/Option.cpp
@@ -10,15 +10,14 @@ void Option::loadIniFile(const std::string& filename)
 
 void Option::loadIniString(const std::string& content)
 {
-    std::string str = de(content);
-    ini_reader_.load(str);
+    ini_reader_.load(content);
     //包含文件仅载入一层
     auto filenames = convert::splitString(getString("load_ini"), ",");
     for (auto filename : filenames)
     {
         if (filename != "")
         {
-            ini_reader_.load(de(convert::readStringFromFile(filename)));
+            ini_reader_.load(convert::readStringFromFile(filename));
         }
     }
 }
@@ -30,46 +29,6 @@ std::string Option::getStringFromSection(const std::string& section, const std::
     convert::replaceAllString(str, "\'", "");
     convert::replaceAllString(str, "\"", "");
     return str;
-}
-
-int Option::getIntFromSection2(const std::string& section, const std::string& name, int default_value)
-{
-    return int(getRealFromSection2(section, name, default_value));
-}
-
-//首先获取公共部分，再以公共部分为默认值获取特殊部分
-double Option::getRealFromSection2(const std::string& section, const std::string& name, double default_value)
-{
-    double a = getReal(name, default_value);
-    a = getRealFromSection(section, name, a);
-    return a;
-}
-
-std::string Option::getStringFromSection2(const std::string& section, const std::string& name, std::string default_value)
-{
-    std::string a = getString(name, default_value);
-    a = getStringFromSection(section, name, a);
-    return a;
-}
-
-void Option::setOptions(std::string section, std::string name_values)
-{
-    setOptions(section, convert::splitString(name_values, ";"));
-}
-
-void Option::setOptions(std::string section, std::vector<std::string> name_values)
-{
-    for (auto name_value : name_values)
-    {
-        convert::replaceAllString(name_value, " ", "");
-        auto p = name_value.find("=");
-        if (p != std::string::npos)
-        {
-            auto name = name_value.substr(0, p);
-            auto value = name_value.substr(p + 1);
-            setOption(section, name, value);
-        }
-    }
 }
 
 void Option::loadSaveValues()
--- src/Option.h
+++ src/Option.h
@@ -41,73 +41,31 @@ public:
     const std::string& getDefautlSection() { return default_section_; }
 
     //从默认section提取
-    int getInt(const std::string& name, int default_value = 0)
+    int getInt(const std::string& key, int default_value = 0)
     {
-        return int(getRealFromSection(default_section_, name, default_value));
+        return int(getRealFromSection(default_section_, key, default_value));
     }
-    double getReal(const std::string& name, double default_value = 0.0)
+    double getReal(const std::string& key, double default_value = 0.0)
     {
-        return getRealFromSection(default_section_, name, default_value);
+        return getRealFromSection(default_section_, key, default_value);
     }
-    std::string getString(const std::string& name, std::string default_value = "")
+    std::string getString(const std::string& key, std::string default_value = "")
     {
-        return getStringFromSection(default_section_, name, default_value);
+        return getStringFromSection(default_section_, key, default_value);
     }
 
     //从指定section提取
-    int getIntFromSection(const std::string& section, const std::string& name, int default_value = 0)
+    int getIntFromSection(const std::string& section, const std::string& key, int default_value = 0)
     {
-        return int(ini_reader_.GetReal(section, name, default_value));
+        return int(ini_reader_.GetReal(section, key, default_value));
     }
-    double getRealFromSection(const std::string& section, const std::string& name, double default_value = 0.0)
+    double getRealFromSection(const std::string& section, const std::string& key, double default_value = 0.0)
     {
-        return ini_reader_.GetReal(section, name, default_value);
+        return ini_reader_.GetReal(section, key, default_value);
     }
-    std::string getStringFromSection(const std::string& section, const std::string& name, std::string default_value = "");
-
-    //先找公共部分，再找section部分
-    int getIntFromSection2(const std::string& section, const std::string& name, int default_value);
-    double getRealFromSection2(const std::string& section, const std::string& name, double default_value);
-    std::string getStringFromSection2(const std::string& section, const std::string& name, std::string default_value);
-
-    bool hasSection(const std::string section) { return ini_reader_.HasSection(section); }
-    bool hasOption(const std::string section, const std::string name) { return ini_reader_.HasOption(section, name); }
-
-    std::vector<std::string> getAllSections() { return ini_reader_.GetAllSections(); }
-    void setOption(std::string section, std::string name, std::string value) { ini_reader_.SetOption(section, name, value); }
-    void setOptions(std::string section, std::string name_values);
-    void setOptions(std::string section, std::vector<std::string> name_values);
+    std::string getStringFromSection(const std::string& section, const std::string& key, std::string default_value = "");
 
     void print() { ini_reader_.print(); }
-
-private:
-    std::function<std::string(std::string)> de_ = nullptr;
-    std::string de(const std::string& content) { if (de_) { return de_(content); } return content; }
-public:
-    void setDe(std::function<std::string(std::string)> de) { de_ = de; }
-    void resetDe() { de_ = nullptr; }
-
-private:
-    template<typename T>
-    T getEnumValue(std::string name, std::map<std::string, T> map)
-    {
-        name = dealString(name);
-        if (map.find(name) != map.end())
-        {
-            return map[name];
-        }
-        else
-        {
-            fprintf(stderr, "Undefined type %s, please check the spelling.\n", name.c_str());
-            return T(0);
-        }
-    }
-
-    template<typename T>
-    void addMemberVector(std::map <std::string, T> map, std::vector<std::pair<std::string, T>> member)
-    {
-
-    }
 
 public:
     int MaxLevel = 30;
