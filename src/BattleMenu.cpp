d76dc09f94ab5e17b71f6992e904d684f272e264
sty <scarsty@gmail.com>
2018-04-08 22:03:19 +0800

    八百里分麾下炙，五十弦翻塞外声 ai评分的随机部分不合理，修改掉

--- src/BattleMenu.cpp
+++ src/BattleMenu.cpp
@@ -306,18 +306,17 @@ int BattleActionMenu::autoSelect(Role* role)
             printf("AI %s: %s ", PotConv::to_read(role->Name).c_str(), PotConv::to_read(getStringFromResult(aa.Action)).c_str());
             if (aa.item) { printf("%s ", PotConv::to_read(aa.item->Name).c_str()); }
             if (aa.magic) { printf("%s ", PotConv::to_read(aa.magic->Name).c_str()); }
-            aa.point += rand.rand();    //用于同分的情况，可以随机选择
-            printf("score %.2f\n", aa.point);
-
+            auto r = rand.rand();    //用于同分的情况，可以随机选择
+            printf("score %.2f(%.2f)\n", aa.point, r);
             //若评分仅有一个随机数的值，说明不在范围内，仅移动并结束
-            if (aa.point < 1)
+            if (aa.point == 0)
             {
                 aa.Action = getResultFromString("結束");
             }
-
-            if (aa.point > max_point)
+            auto p = aa.point + r;
+            if (p > max_point)
             {
-                max_point = aa.point;
+                max_point = p;
                 setAIActionToRole(aa, role);
             }
         }
