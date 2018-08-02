0fdd42a6ad9ceb8f83d5029eddd43f02b7074c9f
three0s <felix.y.tian@gmail.com>
2018-08-01 21:10:07 -0400

    Dao is not knife (#11)
    想当年，金戈铁马，气吞万里如虎
    
    * 编译成功
    
    我觉得问题很多，竟然就编译成了，神奇
    
    * 成功运行一次战斗
    
    * 修了1万个bug
    
    * 分两个文件，手动move constructor，单一触发多特效
    
    触发多特效支持双手互搏计数
    切记如果需要move vector，不能 return { readXX() }; 作list initialization
    修复临时状态没清空bug
    加入几个特效

--- mod/BattleMod.h
+++ mod/BattleMod.h
@@ -2,6 +2,7 @@
 
 #include "Random.h"
 #include "BattleScene.h"
+#include "BattleConfig.h"
 
 #include "yaml-cpp/yaml.h"
 
@@ -10,127 +11,46 @@
 #include <vector>
 #include <utility>
 #include <memory>
+#include <functional>
+#include <optional>
 
 // 先偷懒放一起
 namespace BattleMod {
-    typedef int Param;
-    typedef std::vector<Param> Params;
-
-    // 单个特效，描述特效功能
-    class SpecialEffect {
-    public:
-        SpecialEffect(int id, const std::string& desc);
-        int getId() const;
-        std::string getDescription() const;
-    private:
-        int id_;
-        const std::string description_;
-    };
-
-    // 特效绑定参数
-    class EffectParamPair {
-    public:
-        EffectParamPair(const SpecialEffect& effect, Param p, const std::string& desc);
-        // TODO 再加多参数的。。。 顺便试试variadic template
-        Param getParam0();
-        const Params& getParams();
-        // 叠加方式可不同，现在就都一样吧
-        virtual EffectParamPair & operator+=(const EffectParamPair & rhs);
-
-
-        const SpecialEffect& effect;
-        // 特效解说(显示文字?)
-        const std::string description;
-
-    protected:
-        // 可以有多个参数
-        // 现在每个参数是个int，可以继续配置
-        // 比如根据武功/人物等级增加等
-        // 到时候改上面的typedef
-        Params params_;
-    };
-
-    class ProccableEffect {
-    public:
-        virtual ~ProccableEffect() {};
-        // 这里可以添加其他条件检测
-        // 比如武功组合 是否修炼其他武功
-        // 是否拥有物品，天书数量，内力属性等等等等
-        // proc返回触发的效果，nullptr为触发失败
-        virtual EffectParamPair* proc(RandomDouble&) = 0;
-    };
-
-    enum class ProcProbability {
-        random = 0,
-        distributed = 1
-    };
-
-    // 单个计算
-    class EffectSingle : public ProccableEffect {
-    public:
-        static const ProcProbability type = ProcProbability::random;
-        EffectSingle(int p, EffectParamPair epp);
-        EffectParamPair* proc(RandomDouble&) override;
-    private:
-        int percentProb_;
-        EffectParamPair effectPair_;
-    };
-
-    // 一组特效，可以再继承，其他group类
-    class EffectWeightedGroup : public ProccableEffect {
-    public:
-        static const ProcProbability type = ProcProbability::distributed;
-        EffectParamPair* proc(RandomDouble&) override;
-        EffectWeightedGroup(int total);
-        void addProbEPP(int weight, EffectParamPair epp);
-    private:
-        // 发动参数，和特效
-        std::vector<std::pair<int, EffectParamPair>> group_;
-        int total_;
-    };
-
-    class EffectManager {
-    public:
-        bool hasEffect(int eid);
-        Param getEffectParam0(int eid);
-        Params getAllEffectParams(int eid);
-        EffectParamPair* getEPP(int eid);
-        // 合并管理的特效
-        void unionEffects(const EffectManager& other);
-        // 现在全部采取一个直接加的效果
-        void addEPP(const EffectParamPair& ewp);
-        std::size_t size();
-        void clear();
-    private:
-        std::unordered_map<int, EffectParamPair> epps_;
-    };
-
-    typedef std::vector<std::unique_ptr<ProccableEffect>> Effects;
-    typedef std::unordered_map<int, BattleMod::Effects> EffectsTable;
 
 class BattleModifier : public BattleScene {
     
 private:
     const std::string PATH = "../game/config/battle.yaml";
+
+    // 严禁复制string
+    std::vector<std::string> strPool_;
+
     std::vector<BattleMod::SpecialEffect> effects_;
+    std::vector<BattleStatus> battleStatus_;
 
     // 武功效果，不想碰Magic这个类，修改的东西太多
     // 主动武功效果，必须使用这个武功才行
-    EffectsTable activeAtkMagic_;
-    // 被动武功效果，这个武功存在，攻击就考虑特效
-    EffectsTable passiveAtkMagic_;
+    EffectsTable atkMagic_;
     // 被动挨打的时候的效果
-    EffectsTable passiveDefMagic_;
-    // 武功 拥有既有的全局特效，进入战斗判断一次，一般概率为100%?
-    EffectsTable globalMagic_;
+    EffectsTable defMagic_;
+    // 集气特效
+    EffectsTable speedMagic_;
+    // 回合/行动特效，仅在行动后发动，还有一个在行动前的慢慢添加
+    EffectsTable turnMagic_;
 
-    // 人物效果，不想碰Role这个类，修改的东西太多
-    // 人物 主动攻击特效
+    // 人物 攻击特效
     EffectsTable atkRole_;
-    // 人物 被动挨打特效
+    // 人物 挨打特效
     EffectsTable defRole_;
-    // 人物 参战既有特效，进入战斗判断一次
-    EffectsTable globalRole_;
+    // 人物 集气特效
+    EffectsTable speedRole_;
+    EffectsTable turnRole_;
+
+    // 所有人物共享
+    Effects atkAll_;
+    Effects defAll_;
+    Effects speedAll_;
+    Effects turnAll_;
 
 
     // 管理特效
@@ -138,36 +58,64 @@ private:
     EffectManager atkEffectManager_;
     // 防御特效管理器只有一个，在挨打结束后清空
     EffectManager defEffectManager_;
-    // 以下这两个最好是扔到Role里面，但是我不想碰那个文件
-    // 每个参战人物有一个集气特效管理器，每时序params[1] -= 1，到0时清除
-    // 每个参战人也有一个全局特效管理器
-    // 这里用pid辨识
-    std::unordered_map<int, EffectManager> timeEffectManagers_; // 还没做
-    std::unordered_map<int, EffectManager> globalEffectManagers_;
+    // 集气特效 现在也只需要一个了！
+    EffectManager speedEffectManager_;
+    // 回合 也一个！
+    EffectManager turnEffectManager_;
+
+    std::unordered_map<int, BattleStatusManager> battleStatusManager_;
 
     // simulation对calMagicHurt很不友好
     bool simulation_ = false;
+    // 多重攻击（别称 连击 左右互搏）
+    int multiAtk_ = 0;
 
     void init();
+    Variable readVariable(const YAML::Node& node);
+    std::unique_ptr<Adder> readAdder(const YAML::Node& node);
+    VariableParam readVariableParam(const YAML::Node& node);
+    Conditions readConditions(const YAML::Node& node);
+    Condition readCondition(const YAML::Node& node);
     std::unique_ptr<ProccableEffect> readProccableEffect(const YAML::Node& node);
-    // TODO 确保EffectParamPair movable
-    EffectParamPair readEffectParamPairs(const YAML::Node& node);
+    EffectParamPair readEffectParamPair(const YAML::Node& node);
+    std::vector<EffectParamPair> readEffectParamPairs(const YAML::Node& node);
     void readIntoMapping(const YAML::Node& node, BattleMod::Effects& effects);
-    std::vector<std::string> tryProcAndAddToManager(int id, const EffectsTable& table, EffectManager& manager);
+
+    std::vector<EffectIntsPair> tryProcAndAddToManager(const Effects& list, EffectManager& manager,
+                                                       const Role* attacker, const Role* defender, const Magic* wg);
+    std::vector<EffectIntsPair> tryProcAndAddToManager(int id, const EffectsTable& table, EffectManager& manager,
+                                                       const Role* attacker, const Role* defender, const Magic* wg);
+
+    void applyStatusFromParams(const std::vector<int>& params, Role* target);
+    void setStatusFromParams(const std::vector<int>& params, Role* target);
 
 public:
+
+    // rng 就应该是个全局变量
+    static RandomDouble rng;
+
     // 我决定这不是一个singleton，不用重开游戏就可以测试
     BattleModifier();
     BattleModifier(int id);
 
+    void dealEvent(BP_Event & e) override;
+
+
     // 暂且考虑修改这些函数
     virtual void setRoleInitState(Role* r) override;
+
+    virtual void actUseMagic(Role* r);           //武学
+
     virtual void useMagic(Role* r, Magic* m);
+
     virtual int calMagicHurt(Role* r1, Role* r2, Magic* magic);
     virtual int calMagiclHurtAllEnemies(Role* r, Magic* m, bool simulation = false);    //计算全部人物的伤害
     virtual void showNumberAnimation();
     // 这个是新加的
     virtual Role* semiRealPickOrTick();
+
+    void showMagicNames(const std::vector<std::reference_wrapper<const std::string>>& names);
+
 };
 
 }
