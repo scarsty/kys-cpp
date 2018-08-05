723a4fd0cedb7c16038b40b751d9fbe304fe5cb1
Felix Tian <felix.y.tian@gmail.com>
2018-08-05 10:47:28 +0800

    在剧场门口幽暗的穿衣镜前 透过烟雾凝视着自己
    加入opencc简体转换搜索

--- src/Console.cpp
+++ src/Console.cpp
@@ -4,6 +4,7 @@
 #include "InputAutoComplete.h"
 #include "Save.h"
 #include "MainScene.h"
+#include "opencc/opencc.h"
 
 #include <string>
 #include <vector>
@@ -11,6 +12,7 @@
 #include <unordered_map>
 #include <functional>
 #include <algorithm>
+
 
 Console::Console() {
     std::string code;
@@ -30,16 +32,22 @@ Console::Console() {
         // 致命问题，多个重复场景名，毫无解决方案，应改写可搜索多页MenuText
         std::unordered_map<std::string, int> intResult;
         std::vector<std::string> options;
+        // 这个也可放进PotConv吧，暂时先留这里，我研究点别的再说
+        opencc::SimpleConverter ccConv("s2t.json");
         for (const auto& info : Save::getInstance()->getSubMapInfos()) {
             intResult[info->Name] = info->ID;
             options.emplace_back(info->Name);
         }
-        auto searchF = [&options](const std::string& text) {
+        auto searchF = [&options, &ccConv](const std::string& text) {
             // 理论上可用Trie等加速，或其他更智能距离/LCS算法
+            // 这里搜索时，先转utf8 再反复横跳 text默认因显示原因选择了cp936
+            std::string u8Text = PotConv::cp936toutf8(text);
+            std::string converted = ccConv.Convert(u8Text);
+            std::string realText = PotConv::conv(converted, "utf-8", "cp936");
             std::vector<std::string> results;
             for (auto& opt : options) {
-                auto size = std::min(opt.size(), text.size());
-                if (memcmp(text.c_str(), opt.c_str(), size) == 0) {
+                auto size = std::min(opt.size(), realText.size());
+                if (memcmp(realText.c_str(), opt.c_str(), size) == 0) {
                     results.emplace_back(opt);
                 }
             }
