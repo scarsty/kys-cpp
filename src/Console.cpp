ad3fca82ae40e5aa2fa0319397c7ad680c9220b1
Felix Tian <felix.y.tian@gmail.com>
2018-08-05 23:42:35 -0400

    用抽屉锁住自己的秘密 在喜爱的书上留下批语
    删除没用的InputAutoComplete

--- src/Console.cpp
+++ src/Console.cpp
@@ -2,7 +2,6 @@
 #include "DrawableOnCall.h"
 #include "PotConv.h"
 #include "InputBox.h"
-#include "InputAutoComplete.h"
 #include "Save.h"
 #include "MainScene.h"
 #include "SuperMenuText.h"
@@ -30,50 +29,7 @@ Console::Console() {
     }
     // 捂脸
     code = PotConv::conv(code, "cp936", "utf-8");
-    if (code == u8"teleport1") {
-        // 这是强制传送，什么都不管的
-        // 致命问题，多个重复场景名，毫无解决方案，应改写可搜索多页MenuText
-        std::unordered_map<std::string, int> intResult;
-        std::vector<std::string> options;
-        for (const auto& info : Save::getInstance()->getSubMapInfos()) {
-            intResult[info->Name] = info->ID;
-            options.emplace_back(info->Name);
-        }
-        auto searchF = [&options](const std::string& text) {
-            // 理论上可用Trie等加速，或其他更智能距离/LCS算法
-            std::vector<std::string> results;
-            for (auto& opt : options) {
-                auto size = std::min(opt.size(), text.size());
-                if (memcmp(text.c_str(), opt.c_str(), size) == 0) {
-                    results.emplace_back(opt);
-                }
-            }
-            return results;
-        };
-        auto validateF = [&intResult](const std::string& text) {
-            auto iter = intResult.find(text);
-            if (iter != intResult.end()) {
-                return iter->second;
-            }
-            return -1;
-        };
-        auto limitedOptions = options;
-        limitedOptions.resize(std::min(10u, options.size()));
-        InputAutoComplete autoInput("請輸入傳送地名（可半自動補全）：", 28, limitedOptions);
-        autoInput.setValidation(validateF);
-        autoInput.setSearchFunction(searchF);
-        autoInput.setInputPosition(150, 50);
-        autoInput.run();
-        int id = autoInput.getResult();
-        if (id != -1)
-        {
-            auto scene = Save::getInstance()->getSubMapInfos()[id];
-            
-            MainScene::getInstance()->forceEnterSubScene(id, scene->EntranceX, scene->EntranceY);
-            printf("傳送到%d\n", id);
-        }
-    }
-    else if (code == u8"menutest") {
+    if (code == u8"menutest") {
         std::vector<std::string> generated;
         for (int i = 0; i < 450; i++) {
             generated.push_back("a" + std::to_string(i));
