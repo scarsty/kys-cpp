e0f3fb6893f0327b377912993a14b6dc59db9749
sty <scarsty@gmail.com>
2018-11-28 10:33:05 +0800

    沙上并禽池上暝，云破月来花弄影

--- src/TitleScene.cpp
+++ src/TitleScene.cpp
@@ -112,14 +112,14 @@ void TitleScene::onEntrance()
 {
     Engine::getInstance()->playVideo("../game/movie/1.mp4");
     Audio::getInstance()->playMusic(16);
-    auto tbr = new TextBoxRoll();
+    //auto tbr = new TextBoxRoll();
 
-    TextBoxRoll::TextColorLines texts;
-    for (int i = 0; i <= 10; i++)
-    {
-        texts.push_back({ { { 0, 0, 0, 255 }, "sb" + std::to_string(250 + i) } });
-    }
-    tbr->setTexts(texts);
-    tbr->setRollLine(5);
-    menu_->addChild(tbr, -100, -100);
+    //TextBoxRoll::TextColorLines texts;
+    //for (int i = 0; i <= 10; i++)
+    //{
+    //    texts.push_back({ { { 0, 0, 0, 255 }, "sb" + std::to_string(250 + i) } });
+    //}
+    //tbr->setTexts(texts);
+    //tbr->setRollLine(5);
+    //menu_->addChild(tbr, -100, -100);
 }
