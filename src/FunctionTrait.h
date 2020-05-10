c05b47dcc51c31ec62b288ab661c04dea7fae34c
sty <scarsty@gmail.com>
2020-05-11 00:22:21 +0800

    满堂花醉三千客，一剑霜寒十四州 模板里都用typename

--- src/FunctionTrait.h
+++ src/FunctionTrait.h
@@ -2,20 +2,24 @@
 #include <cstddef>
 #include <type_traits>
 
-template <class F, typename C, typename... Args>
-struct arg_counter { };
+template <typename F, typename C, typename... Args>
+struct arg_counter
+{
+};
 
 template <typename F, typename C, typename... Args>
-struct arg_counter<F(C::*)(Args...), C>
+struct arg_counter<F (C::*)(Args...), C>
 {
     static constexpr std::size_t value = sizeof...(Args);
 };
 
-template <class F, typename C, typename T, typename... Args>
-struct check_return_type { };
+template <typename F, typename C, typename T, typename... Args>
+struct check_return_type
+{
+};
 
 template <typename F, typename C, typename T, typename... Args>
-struct check_return_type<F(C::*)(Args...), C, T>
+struct check_return_type<F (C::*)(Args...), C, T>
 {
     static constexpr bool value = std::is_same<F, T>::value;
 };
