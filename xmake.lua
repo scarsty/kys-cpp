79f044203c1b5f7e31393491e39c32973c89ae93
wsw0108 <wsw0108@gmail.com>
2022-11-26 19:16:02 +0800

    增加bass与sdl-mixer切换的选项

--- xmake.lua
+++ xmake.lua
@@ -6,12 +6,28 @@ set_languages("cxx20")
 
 set_warnings("all", "warning")
 
+option("audio")
+    set_default("sdl")
+    set_values("sdl", "bass")
+option_end()
+
+option("bass-dir")
+    set_default("")
+option_end()
+
+option("bassmidi-dir")
+    set_default("")
+option_end()
+
 if is_plat("windows") then
     add_defines("NOMINMAX")
     add_defines("_CRT_SECURE_NO_WARNINGS")
 end
 
-add_requires("libsdl", "libsdl_ttf", "libsdl_image", "libsdl_mixer")
+add_requires("libsdl", "libsdl_ttf", "libsdl_image")
+if get_config("audio") == "sdl" then
+    add_requires("libsdl_mixer")
+end
 add_requires("lua")
 add_requires("opencc")
 add_requires("sqlite3")
@@ -32,12 +48,17 @@ target("kys")
     add_includedirs("mlcc", "others", "local/include")
     if is_plat("windows") then
         add_cxflags("/utf-8")
-        -- add_cxflags("/bigobj")
     end
-    add_packages("libsdl", "libsdl_ttf", "libsdl_image", "libsdl_mixer")
+    add_packages("libsdl", "libsdl_ttf", "libsdl_image")
     add_packages("lua", "opencc", "sqlite3", "libiconv", "asio", "picosha2", "yaml-cpp", "libzip")
-    -- add_linkdirs("local/lib/x64")
-    add_defines("USE_SDL_MIXER_AUDIO")
-    -- add_links("bass", "bassmidi")
+    if get_config("audio") == "sdl" then
+        add_defines("USE_SDL_MIXER_AUDIO")
+        add_packages("libsdl_mixer")
+    else
+        add_links("bass", "bassmidi")
+        add_includedirs("$(bass-dir)/c", "$(bassmidi-dir)/c")
+        add_linkdirs("$(bass-dir)/c/x64", "$(bassmidi-dir)/c/x64")
+        add_runenvs("PATH", "$(bass-dir)/x64", "$(bassmidi-dir)/x64")
+    end
     -- add_defines("WITH_SMALLPOT")
     -- add_packages("smallpot")
