40211fcd2d6eacfe71a26b09044a8d26b365b91e
sty <scarsty@gmail.com>
2024-07-15 15:06:19 +0800

    天地皆得一，澹然四海清 增加选择战斗模式

--- src/UISystem.cpp
+++ src/UISystem.cpp
@@ -1,15 +1,15 @@
 #include "UISystem.h"
-#include "UISave.h"
-#include "Event.h"
-#include "Script.h"
 #include "Engine.h"
+#include "Event.h"
 #include "GameUtil.h"
+#include "Script.h"
 #include "UIKeyConfig.h"
+#include "UISave.h"
 
 UISystem::UISystem()
 {
     title_ = std::make_shared<MenuText>();
-    title_->setStrings({ "讀取進度", "保存進度", "我的代碼","鍵位配置", "離開遊戲" });
+    title_->setStrings({ "讀取進度", "保存進度", "我的代碼", "戰鬥模式", "離開遊戲" });
     title_->setFontSize(24);
     title_->arrange(100, 50, 120, 0);
     title_->setLRStyle(1);
@@ -23,13 +23,14 @@ UISystem::~UISystem()
 void UISystem::onPressedOK()
 {
     result_ = -1;
+    int x = 400 + 120 * title_->getResult();
     if (title_->getResult() == 0)
     {
         //读档
         auto ui_load = std::make_shared<UISave>();
         ui_load->setMode(0);
         ui_load->setFontSize(22);
-        result_ = ui_load->runAtPosition(400, 100);
+        result_ = ui_load->runAtPosition(x, 100);
     }
     else if (title_->getResult() == 1)
     {
@@ -37,7 +38,7 @@ void UISystem::onPressedOK()
         auto ui_save = std::make_shared<UISave>();
         ui_save->setMode(1);
         ui_save->setFontSize(22);
-        result_ = ui_save->runAtPosition(520, 100);
+        result_ = ui_save->runAtPosition(x, 100);
     }
     else if (title_->getResult() == 2)
     {
@@ -45,9 +46,26 @@ void UISystem::onPressedOK()
     }
     else if (title_->getResult() == 3)
     {
-        auto menu = std::make_shared<UIKeyConfig>();
-        menu->setPosition(200, 200);
-        menu->run();
+        std::vector<std::string> modes = { "回合制", "半即時回合制", "黑帝斯", "隻狼" };
+        int mode = GameUtil::getInstance()->getInt("game", "battle_mode", 0);
+        if (mode >= 0 && mode < modes.size())
+        {
+            modes[mode] += "（當前）";
+        }
+        auto menu = std::make_shared<MenuText>();
+        menu->setStrings(modes);
+        menu->setFontSize(24);
+        menu->arrange(0, 0, 0, 40);
+        result_ = menu->runAtPosition(x, 100);
+        if (result_ >= 0)
+        {
+            GameUtil::getInstance()->setKey("game", "battle_mode", std::to_string(result_));
+            auto text = std::make_shared<TextBox>();
+            mode = GameUtil::getInstance()->getInt("game", "battle_mode", 0);
+            text->setText("戰鬥模式已設置為：" + modes[mode]);
+            text->setFontSize(24);
+            text->runAtPosition(x, 100);
+        }
     }
     else if (title_->getResult() == title_->getChildCount() - 1)
     {
