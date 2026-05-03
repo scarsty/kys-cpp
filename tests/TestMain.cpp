b6b6e0175cd9b94b9f6fa232faf9ebf0ad2bb51d
fyt000 <9795292+fyt000@users.noreply.github.com>
2026-05-03 16:49:57 +0800

    Route test abort reporting to stderr

---
+++ tests/TestMain.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_session.hpp>
+
+#ifdef _WIN32
+#include <cstdlib>
+#include <crtdbg.h>
+#include <windows.h>
+#endif
+
+namespace
+{
+
+void configureAbortReporting()
+{
+#ifdef _WIN32
+    SetErrorMode(SEM_FAILCRITICALERRORS | SEM_NOGPFAULTERRORBOX | SEM_NOOPENFILEERRORBOX);
+    _set_error_mode(_OUT_TO_STDERR);
+    _set_abort_behavior(0, _WRITE_ABORT_MSG | _CALL_REPORTFAULT);
+
+    _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
+    _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
+    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+#endif
+}
+
+}  // namespace
+
+int main(int argc, char* argv[])
+{
+    configureAbortReporting();
+    return Catch::Session().run(argc, argv);
+}
